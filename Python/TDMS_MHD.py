15a722dcc0e554cafc62afa192e64670514007fb
Lee R. Aspitarte <aspitarl@research.local>
2018-02-16 10:52:00 -0800

    split class structure for numpy and pandas

--- Python/TDMS_MHD.py
+++ Python/TDMS_MHD.py
@@ -10,17 +10,14 @@ class TDMS():
         self.samples = []
         self.pathnames = []
         self.imports = {}
-        self.data = {}
-        self.tracks = {}
+
         
         ##samples is a list of strings, the samples names
         ##pathnames is the full pathnames to get to each tdms file (there maybe multiple tdms files associated to different samples)
         ##imports is a dictionary, using the pathnames as the word and the imported tdms file as its definition
         ##           this step is necessary as importing the data is the longest process, but pulling data to arrays is almost immediate
-        ##data is a dictionary, using the pathnames as the word and a 2D array with all data stored, useful for plotting
-        ##tracks is meant to track what data has already been pulled from the imported file, and track what position in the data array certain 
-        ##data are
-        ##           tracks uses channelnames as the word and an integer as its definition
+    
+    #TODO: This needs to be make more general. i.e. not rely on specific folder structure
     def set_pathnames(self, path, filestr):
         samples = self.samples
         pathnames = self.pathnames
@@ -47,14 +44,8 @@ class TDMS():
                 imports[pathname] = TF(pathname)
         
         self.imports = imports
-        self.get_times()
-        self.createref()
         return
 
-    ##imports tdms files that contain the given string. for example: the molybdenum rod experiments had 9mm, 11mm, and 13mm runs
-    ##folder and file paths were organized as such
-    ##runs get_times(), makes it easier to stack arrays of data and line them up with time
-    
     def createref(self,):
         dic = {}
         for impor in self.imports:
@@ -68,17 +59,54 @@ class TDMS():
                 dic[group] = np.append(dic[group], channelname)
         self.groups = dic
         return
+
+    ##imports tdms files that contain the given string. for example: the molybdenum rod experiments had 9mm, 11mm, and 13mm runs
+    ##folder and file paths were organized as such
+    ##runs get_times(), makes it easier to stack arrays of data and line them up with time
+    
+    def resize(self, arr, length):
+        if len(arr)>length:
+            arr = arr[1:]
+        elif len(arr)<length:
+            arr = np.append(arr, 0)
+        else:
+            print("error")
+        return arr   
+    
+class TDMS_pd(TDMS):
+    def __init__(self, ):
+        TDMS.__init__(self)
+        self.datapdframes = {}
+        self.datapseries = {}
+        
+    
+    def get_data_pdframe(self,file, groupname):
+        groups = file.groups()
+        for group in groups:
+            if(group == groupname):
+                data = file.object(groupname).as_dataframe()
+        
+        self.datapdframes[groupname] = data
+        return
+    
+class TDMS_np(TDMS):
+    def __init__(self):
+        TDMS.__init__(self)
+        self.data = {}
+        self.tracks = {}
+        
+        ##data is a dictionary, using the pathnames as the word and a 2D array with all data stored, useful for plotting
+        ##tracks is meant to track what data has already been pulled from the imported file, and track what position in the data array certain 
+        ##data are
+        ##           tracks uses channelnames as the word and an integer as its definition
     
     def get_custom(self, channelname):
         tctrack = channelname
         self.get_data(tctrack)
         return
         ##allows you to get data from a custom channel, given an input channel name
-        
-        
-        
+          
     def get_data(self, track, group=10):
-        data = self.data
         imports = self.imports
         tracks = self.tracks
         groups = self.groups
@@ -116,11 +144,3 @@ class TDMS():
         ##as implied, stores data. stacks the array with time, updates the data dictionary, and stores data position in tracks
         ##the else statement prevents the data arrays from storing duplicate data
         
-    def resize(self, arr, length):
-        if len(arr)>length:
-            arr = arr[1:]
-        elif len(arr)<length:
-            arr = np.append(arr, 0)
-        else:
-            print("error")
-        return arr   
