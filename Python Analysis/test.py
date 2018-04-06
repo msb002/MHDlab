306379bfdb2d4b95dab996313f1aab756968a8b1
lee <aspitarte@gmail.com>
2018-04-06 09:58:40 -0700

    initial commit

---
+++ Python Analysis/test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 01:37:11 2018
+
+@author: aspit
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+fig, ax = plt.subplots()
+line, = ax.plot(np.random.rand(10))
+ax.set_ylim(0, 1)
+
+
+def update(data):
+    line.set_ydata(data)
+    return line,
+
+
+def data_gen():
+    while True:
+        yield np.random.rand(10)
+
+ani = animation.FuncAnimation(fig, update, data_gen, interval=100)
+plt.show()
