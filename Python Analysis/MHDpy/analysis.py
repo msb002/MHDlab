8ee6e4a8b11592cfe3899219bd6310871fb414bc
aspitarl <aspitarte@gmail.com>
2018-05-20 16:59:28 -0700

    Update vapor tube notebook Make PL plot a class and update in both HVOF and Vaportube notebok Add waterfall to plotting

---
+++ Python Analysis/MHDpy/analysis.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 20 11:52:12 2018
+
+@author: aspit
+"""
+import numpy as np
+
+
+def find_nearest(a, a0):
+    "Element in nd array `a` closest to the scalar value `a0`"
+    idx = np.abs(a - a0).argmin()
+    return idx
+
+
+def PL_peakmax(data, wavelength, wlmin,wlmax):
+    idx_l = find_nearest(wavelength,wlmin)
+    idx_r = find_nearest(wavelength,wlmax)
+    
+    wavelength_cut = wavelength[idx_l:idx_r]
+    
+    data_cut = data[idx_l:idx_r]
+    
+    data_cut_max = []
+    
+    for frame in data_cut:
+        maximum = data_cut[frame].max()
+        data_cut_max = np.append(data_cut_max, maximum)
+        
+    return data_cut_max, wavelength_cut
+
--- Python Analysis/MHDpy/plotting.py
+++ Python Analysis/MHDpy/plotting.py
@@ -11,46 +11,49 @@ from matplotlib import animation, rc
 from IPython.display import HTML
 
 
-def PL(Laser, Lasertime, peak1, peak2, PLtime):
-    fig, ax1 = plt.subplots(figsize = (8,6))
-    ln1 = ax1.plot(PLtime, peak1, 'b.', label = '767 peak')
-    ln2 = ax1.plot(PLtime, peak2, 'g.', label = '770 peak')
+class PLplot():
     
-    ax2 = ax1.twinx()
+    def __init__(self,Laser, Lasertime, peak1, peak2, PLtime):
+        self.fig, self.ax1 = plt.subplots(figsize = (8,6))
+        ln1 = self.ax1.plot(PLtime, peak1, 'b.', label = 'Peak 1')
+        ln2 = self.ax1.plot(PLtime, peak2, 'g.', label = 'Peak 2')
+        
+        self.ax2 = self.ax1.twinx()
+        
+        self.ax2.set_ylabel("Laser Intensity (Normalized)")
+        self.ax2.tick_params('y')
+        
+        plt.legend()
+        
+        ln3 = self.ax2.plot(Lasertime, Laser, 'r', label = 'Laser profile')
+        
+        self.ax1.set_xlabel("Gate Delay (ns)")
+        # Make the y-axis label, ticks and tick labels match the line color.
+        self.ax1.set_ylabel("$Delta$ PL Intensity (Normalized)")
+        self.ax1.tick_params('y')
+        
+        
+        
+        
+        self.fig.suptitle('Potassium HVOF PL', y = 1)
+        self.fig.tight_layout()
+        
+        lns = ln1+ln2+ln3
+        labs = [l.get_label() for l in lns]
+        self.ax1.legend(lns, labs, loc=0)
+        
+        self.ax2.set_yscale('log')
+        self.ax1.set_yscale('log')
+        
+        #self.ax1.set_ylim((0.00001),2)
+        #self.ax2.set_ylim((0.00001),2)
+        #self.ax1.set_xlim(-20,60)
     
-    ax2.set_ylabel("Laser Intensity (Normalized)")
-    ax2.tick_params('y')
     
-    plt.legend()
-    
-    ln3 = ax2.plot(Lasertime, Laser, 'r', label = 'Laser profile')
-    
-    ax1.set_xlabel("Gate Delay (ns)")
-    # Make the y-axis label, ticks and tick labels match the line color.
-    ax1.set_ylabel("$\\Delta$ PL Intensity (Normalized)")
-    ax1.tick_params('y')
-    
-    
-    
-    
-    plt.title("Potassium HVOF PL")
-    fig.tight_layout()
-    
-    lns = ln1+ln2+ln3
-    labs = [l.get_label() for l in lns]
-    ax1.legend(lns, labs, loc=0)
-    
-    ax2.set_yscale('log')
-    ax1.set_yscale('log')
-    
-    #ax1.set_ylim((0.00001),2)
-    #ax2.set_ylim((0.00001),2)
-    ax1.set_xlim(-20,60)
-    
-    plt.show()
 
 
-def spectral_anim(RawData_Frames, spectra_wl,spectra_time_off ):
+
+def spectral_anim(RawData_Frames, spectra_wl,spectra_time_off , interval = 15):
     """
     Matplotlib Animation Example
     
@@ -100,7 +103,7 @@ def spectral_anim(RawData_Frames, spectra_wl,spectra_time_off ):
     
     # call the animator.  blit=True means only re-draw the parts that have changed.
     anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                   frames=30, interval=15, blit=True)
+                                   frames=len(spectra_time_off), interval = interval, blit=True)
     
     
     #TML(anim.to_html5_video())
@@ -109,3 +112,48 @@ def spectral_anim(RawData_Frames, spectra_wl,spectra_time_off ):
     rc('animation', html='html5')
     
     return anim
+
+
+
+
+def PL_waterfall(RawData_Frames, spectra_wl,spectra_time_off):
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib.collections import PolyCollection
+    from matplotlib import colors as mcolors
+    
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    
+    
+    def cc(arg):
+        return mcolors.to_rgba(arg, alpha=0.6)
+    
+    xs = spectra_wl
+    verts = []
+    zs = spectra_time_off
+    i=0
+    numexposures = len(zs)- 1
+    for z in zs:
+        ys = RawData_Frames.iloc[:,numexposures-i].as_matrix()
+        i=i+1
+        ys[0], ys[-1] = 0, 0
+        verts.append(list(zip(xs, ys)))
+        
+        
+    poly = PolyCollection(verts, facecolors = (1,1,1,0), edgecolors=(0,0,1,0.5))
+    #poly.set_alpha(0.2)
+    #face_color = [0, 0, 0] # alternative: matplotlib.colors.rgb2hex([0.5, 0.5, 1])
+    #poly.set_edgecolor(face_color)
+    ax.add_collection3d(poly, zs=zs, zdir='y')
+    
+    
+    ax.set_xlabel('X')
+    ax.set_xlim3d(xs[0], xs[-1])
+    ax.set_ylabel('Y')
+    ax.set_ylim3d(zs[0], zs[-1])
+    ax.set_zlabel('Z')
+    ax.set_zlim3d(0, RawData_Frames.max().max())
+    
+    
+    ax.view_init(20, 300)
+    plt.show()
