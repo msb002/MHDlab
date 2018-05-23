9c906e4523538544dad8d24fe4019c95055b7dbb
aspitarl <aspitarte@gmail.com>
2018-05-22 21:40:01 -0700

    modularize some analysis and make plotting iterative through NDF

--- Python Analysis/MHDpy/analysis.py
+++ Python Analysis/MHDpy/analysis.py
@@ -29,3 +29,16 @@ def PL_peakmax(data, wavelength, wlmin,wlmax):
         
     return data_cut_max, wavelength_cut, data_cut
 
+def PL_fit_powerdep(data, fits, NDF,idx_l,idx_r):
+    timedata = data['Time'][NDF]
+    data_p1_ln = np.log(data['Data_p1_max_norm'][NDF])
+    fits['Fit_p1_param'][NDF] =np.polyfit(timedata[idx_l:idx_r], data_p1_ln[idx_l:idx_r], 1)
+    fits['Fit_p1_time'][NDF] = np.linspace(timedata[idx_l], timedata[idx_r], 100)
+    fits['Fit_p1'][NDF] = np.poly1d(fits['Fit_p1_param'][NDF])
+    fits['Fit_p1'][NDF] = np.exp(fits['Fit_p1'][NDF](fits['Fit_p1_time'][NDF]))
+    
+    data_p2_ln = np.log(data['Data_p2_max_norm'][NDF]) 
+    fits['Fit_p2_param'][NDF] =np.polyfit(timedata[idx_l:idx_r], data_p2_ln[idx_l:idx_r], 1)
+    fits['Fit_p2_time'][NDF] = np.linspace(timedata[idx_l], timedata[idx_r], 100)
+    fits['Fit_p2'][NDF] = np.poly1d(fits['Fit_p2_param'][NDF])
+    fits['Fit_p2'][NDF] = np.exp(fits['Fit_p2'][NDF](fits['Fit_p2_time'][NDF]))
