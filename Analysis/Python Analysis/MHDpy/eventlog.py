5e7fb892ca7d6effe6b0361c379e3e6444d7cec5
Lee Aspitarte <aspitarte@gmail.com>
2018-08-09 19:29:55 -0700

    get event log json working with test case info changes.

--- Analysis/Python Analysis/MHDpy/eventlog.py
+++ Analysis/Python Analysis/MHDpy/eventlog.py
@@ -6,5 +6,37 @@ This is a temporary script file.
 """
 import json
 
-def TestCaseInfoChange(project, subfolder, filename, measurementnum, time):
-    print(measurementnum)
+Eventlogfile = ""
+
+def fixeventlogfile():
+    global Eventlogfile
+    if isinstance(Eventlogfile,bytes): #The labview addin passes a bytes instead of string. 
+        Eventlogfile = Eventlogfile.decode("utf-8")
+
+
+def TestCaseInfoChange(TestDataInfo, time):
+    for idx, string in  enumerate(TestDataInfo):
+        TestDataInfo[idx] = string.decode("utf-8")
+    
+    project = TestDataInfo[0]
+    subfolder = TestDataInfo[1]
+    filename = TestDataInfo[2]
+    measurementnumber = TestDataInfo[3]
+
+    event = {
+    "dt": time,
+    "event": {
+        "TestCaseInfoChange": {
+            "project": project,
+            "subfolder": subfolder,
+            "filename": filename,
+            "measurementnumber": measurementnumber 
+            }
+        }
+    }
+
+    print(event)
+
+    with open(Eventlogfile, "a") as write_file:
+        json.dump(event, write_file)
+        write_file.write('\n')
