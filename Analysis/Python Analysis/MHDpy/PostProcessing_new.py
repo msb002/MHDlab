b0c3d9a3918bfe57a1477113ccb81471c9d4a4a6
Lee Aspitarte <aspitarte@gmail.com>
2018-08-15 22:18:12 -0700

    get tdms file cutting working

--- Analysis/Python Analysis/MHDpy/PostProcessing_new.py
+++ Analysis/Python Analysis/MHDpy/PostProcessing_new.py
@@ -9,14 +9,20 @@ This program pulls data from the logging files to copy the 'control VI' files
 
 
 import numpy as np
-from nptdms import TdmsFile as TF
 import time
 import pandas as pd
 import os
 import sys
+from nptdms import TdmsFile as TF
 from nptdms import TdmsWriter, RootObject, GroupObject, ChannelObject
 import datetime
 import pytz
+
+#Make sure Python Analysis folder in in PYTHONPATH and import the MHDpy module
+PythonAnalysisPath = 'C:\\Users\\aspit\\Git\\MHDLab\\Python Analysis'
+if not PythonAnalysisPath in sys.path:
+    sys.path.append(PythonAnalysisPath)
+
 
 def nearest_timeind(timearray, pivot):   
     diff = timearray - pivot
@@ -30,39 +36,62 @@ def combine_channels(Fileinfo,group,channel):
         combined = np.append(combined,data)
     return combined
 
+def cut_tdms_file(filepath,timestamps,samplerate):
+    if isinstance(filepath,bytes): #The labview addin passes a bytes instead of string. 
+        filepath = filepath.decode("utf-8")
+
+    Logfiletdms = TF(filepath)
+
+    timedata = np.array(Logfiletdms.channel_data('Global','Time'))
+
+    #time1 = timedata[10]
+    #time2 = timedata[20]
+
+    time1 = datetime.datetime.utcfromtimestamp(timestamps[0]).replace(tzinfo=pytz.UTC)
+    time2 = datetime.datetime.utcfromtimestamp(timestamps[1]).replace(tzinfo=pytz.UTC)
+
+    idx1 = nearest_timeind(timedata,time1)
+    idx2 = nearest_timeind(timedata,time2)
+
+    data = Logfiletdms.as_dataframe()
+
+    data = data[idx1:idx2][:]
+
+    f, ext = os.path.splitext(filepath)
+
+    newfile = f + '_cut.tdms'
+
+    root_object = RootObject(properties={
+    "prop1": "foo",
+    "prop2": 3,
+    })
+    group_object = GroupObject("group_1", properties={
+        "prop1": 1.2345,
+        "prop2": False,
+    })
+    
+    
+    channel_object = ChannelObject("group_1", "channel_1", data[:]["/'Data'/'Current_1'"], properties={})
+
+    with TdmsWriter(newfile) as tdms_writer:
+        tdms_writer.write_segment([
+            root_object,
+            group_object,
+            channel_object])
+
+
+DataPath = 'C:/Labview Test Data/2018-08-13/Sensors/Log_Sensors_DAQ_0.tdms'
+
+Logfiletdms = TF(DataPath)
+
+timedata = np.array(Logfiletdms.channel_data('Global','Time'))
 
 
 
-#Make sure Python Analysis folder in in PYTHONPATH and import the MHDpy module
-PythonAnalysisPath = 'C:\\Users\\aspit\\Git\\MHDLab\\Python Analysis'
-if not PythonAnalysisPath in sys.path:
-    sys.path.append(PythonAnalysisPath)
+timestamps = [1534213304, 1534213317]
 
+time1 = datetime.datetime.utcfromtimestamp(timestamps[0]).replace(tzinfo=pytz.UTC)
 
-samplerate = 100
+time2 = datetime.datetime.utcfromtimestamp(timestamps[1]).replace(tzinfo=pytz.UTC)
 
-DataPath = 'C:/Labview Test Data/2018-08-13/Sensors'
-os.chdir(DataPath)
-
-Fileinfo = []
-
-for (dirpath, dirnames, filenames) in os.walk(DataPath):
-    for f in filenames:
-        f, ext = os.path.splitext(f)
-        suffixstr = "_Log"
-        if(ext == '.tdms'):
-            Logfiletdms = TF(os.path.join(dirpath,f+ext))
-            Fileinfo.append((f,Logfiletdms))
-    break
-
-
-Fileinfo = pd.DataFrame(Fileinfo, columns = ['filename','TDMSfile'])
-
-timedata = combine_channels(Fileinfo,'Global','Time')
-
-time1 = datetime.datetime(2018, 8, 14, 2, 21, 45,tzinfo=pytz.timezone('UTC'))
-time2 = datetime.datetime(2018, 8, 14, 4, 34, 20,tzinfo=pytz.timezone('UTC'))
-
-idx1 = nearest_timeind(timedata,time2)
-
-#time.strptime()
+data = cut_tdms_file(DataPath,timestamps,100)
