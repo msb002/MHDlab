4b40966b2f2166534f039efeac5db147b15ae253
Lee Aspitarte <aspitarte@gmail.com>
2018-08-16 12:32:07 -0700

    add ability to find the corresponding event, not finished
    it is currently finding the wrong event, need to correct algorithm. but
    correctly passing testcaseinfo to the cutting vi.

--- Analysis/Python Analysis/MHDpy/PostProcessing.py
+++ Analysis/Python Analysis/MHDpy/PostProcessing.py
@@ -3,97 +3,142 @@
 This program pulls data from the logging files to copy the 'control VI' files
 """
 
+"""
+This program pulls data from the logging files to copy the 'control VI' files
+"""
+
 
 import numpy as np
-
-
-from nptdms import TdmsFile as TF
-from nptdms import TdmsWriter, RootObject, GroupObject, ChannelObject
-
+import time
 import pandas as pd
-
 import os
 import sys
-
+from nptdms import TdmsFile as TF
 from nptdms import TdmsWriter, RootObject, GroupObject, ChannelObject
-
-def nearest_ind(items, pivot):
-    time_diff = np.abs([date - pivot for date in items])
-    return time_diff.argmin(0)
-
+import datetime
+import pytz
+import json
 
 #Make sure Python Analysis folder in in PYTHONPATH and import the MHDpy module
 PythonAnalysisPath = 'C:\\Users\\aspit\\Git\\MHDLab\\Python Analysis'
 if not PythonAnalysisPath in sys.path:
     sys.path.append(PythonAnalysisPath)
 
-from nptdms import TdmsFile as TF
 
-samplerate = 100
+def labview_to_unix(timestamps):
+    newtimestamps = list(map(lambda x: x -2082844800 ,timestamps))
+    return newtimestamps
 
+def nearest_timeind(timearray, pivot):   
+    diff = timearray - pivot
+    seconds = np.array(list(map(lambda x: abs(x.total_seconds()),diff)))
+    return seconds.argmin()
 
-DataPath = 'C:/Labview Test Data/2018-08-08'
+def combine_channels(Fileinfo,group,channel):
+    combined = np.empty(0)
+    for TDMSfile in Fileinfo['TDMSfile']:
+        data = TDMSfile.channel_data(group,channel)
+        combined = np.append(combined,data)
+    return combined
 
-os.chdir(DataPath)
+def cut_tdms_file(filepath,timestamps,samplerate):
+    if isinstance(filepath,bytes): #The labview addin passes a bytes instead of string. 
+        filepath = filepath.decode("utf-8")
 
-Logfiles = []
-ProjectFolders = []
-for (dirpath, dirnames, filenames) in os.walk(DataPath):
-    for f in filenames:
-        f, ext = os.path.splitext(f)
-        suffixstr = "_Log"
-        if(ext == '.tdms'):
-            f = f[:len(f)-len(suffixstr)]
-            Logfiles.extend([f])
-    ProjectFolders.extend(dirnames)
-    break
+    timestamps = labview_to_unix(timestamps)
 
+    Logfiletdms = TF(filepath)
 
-Fileinfo = []
-for Project in ProjectFolders:
-    for dirpath,_,filenames in os.walk(os.path.join(DataPath, Project)):
-        for f in filenames:
-            fname, ext = os.path.splitext(f)
-            prefixstr = "Control"
-            if(fname.startswith(prefixstr) and ext == '.tdms'):
-                path = os.path.join(dirpath,f)
-                controltdms = TF(os.path.join(dirpath,f))
-                timedata = controltdms.channel_data('Global', 'Time')
-                suf = fname[len(prefixstr):]
-                Fileinfo.append((suf,dirpath,timedata))
+    timedata = np.array(Logfiletdms.channel_data('Global','Time'))
 
-Fileinfo = pd.DataFrame(Fileinfo, columns = ['suffix','folder','time'])
+    #time1 = timedata[10]
+    #time2 = timedata[20]
 
+    time1 = datetime.datetime.utcfromtimestamp(timestamps[0]).replace(tzinfo=pytz.UTC)
+    time2 = datetime.datetime.utcfromtimestamp(timestamps[1]).replace(tzinfo=pytz.UTC)
 
+    idx1 = nearest_timeind(timedata,time1)
+    idx2 = nearest_timeind(timedata,time2)
 
+    data = Logfiletdms.as_dataframe()
 
+    data = data[idx1*samplerate:idx2*samplerate][:]
 
+    f, ext = os.path.splitext(filepath)
 
-root_object = RootObject()
-group_object = GroupObject("group_1", properties={
-    "prop1": 1.2345,
-    "prop2": False,
-})
-data = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
-channel_object = ChannelObject("group_1", "channel_1", data, properties={})
+    newfile = f + '_cut.tdms'
 
+    root_object = RootObject(properties={
+    "prop1": "foo",
+    "prop2": 3,
+    })
 
 
-i = 0
-for Logfile in Logfiles:
-    Logfiletdms = TF(os.path.join(DataPath,Logfile + '_Log.tdms'))
-    Logfiledf = Logfiletdms.as_dataframe()
-    Logfiletime = Logfiletdms.channel_data('Time','Time')
-    for Fileinforow in Fileinfo.iterrows():
-        mintimeidx = nearest_ind(Logfiletime,Fileinforow[1]['time'][0])
-        maxtimeidx = nearest_ind(Logfiletime,Fileinforow[1]['time'][-1])
+    with TdmsWriter(newfile) as tdms_writer:
+        channel_object = ChannelObject('Global', 'Time', timedata[idx1:idx2], properties={})
         
-        
-        
-        newfile = os.path.join(Fileinforow[1]['folder'], Logfile + Fileinforow[1]['suffix'] + '.tdms')
-        with TdmsWriter(newfile) as tdms_writer:
-            tdms_writer.write_segment([
-                root_object,
-                group_object,
-                channel_object])
+        tdms_writer.write_segment([
+            root_object,
+            channel_object])
 
+        for channelstr in data.columns:
+            strsplit = channelstr.split('/')
+            group = strsplit[1].replace("\'","")
+            channel = strsplit[2].replace("\'","")
+            if(group == 'Data'):
+                channel_object = ChannelObject('Data' , channel, data[channelstr].as_matrix(), properties={})
+                tdms_writer.write_segment([
+                    root_object,
+                    channel_object])
+
+class jsoninfo():
+    def __init__(self,filepath):
+        with open(filepath) as fp:
+            self.jsonfile = json.load(fp)
+
+    def gettestcaseinfo(self,timestamps):
+        timestamps = labview_to_unix(timestamps)
+        time1 = timestamps[0]
+        print(time1)
+
+        times = []
+        testcaseinfo = []
+        for event in self.jsonfile:
+            if event['event']['type'] == 'TestCaseInfoChange':
+                testcaseinfo.append(event['event']['event info'])
+                times.append(event['dt'])
+        i=0
+        for time in times:
+            if(time>time1):
+                break
+            else:
+                i=i+1
+        print(i)
+        tci = testcaseinfo[i]
+        testcaseinfoarray = [tci['project'],tci['subfolder'],tci['filename'],tci['measurementnumber']]
+        return testcaseinfoarray
+            
+
+
+
+
+
+# DataPath = 'C:/Labview Test Data/2018-08-13/Sensors/Log_Sensors_DAQ_0.tdms'
+
+jsonpath = 'C:/Labview Test Data/2018-08-13/Eventlog.json'
+
+test = jsoninfo(jsonpath)
+
+# Logfiletdms = TF(DataPath)
+
+# timedata = np.array(Logfiletdms.channel_data('Global','Time'))
+
+
+
+# timestamps = [1534213306, 1534213315]
+
+# time1 = datetime.datetime.utcfromtimestamp(timestamps[0]).replace(tzinfo=pytz.UTC)
+
+# time2 = datetime.datetime.utcfromtimestamp(timestamps[1]).replace(tzinfo=pytz.UTC)
+
+# data = cut_tdms_file(DataPath,timestamps,100)
