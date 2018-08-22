d38ee253ab8ffd4f95a648b52c4cd747da7c3c25
Lee Aspitarte <aspitarte@gmail.com>
2018-08-22 13:50:33 -0700

    get updating display of groups and channels

--- Analysis/Post Processing/PostProcessingUI.py
+++ Analysis/Post Processing/PostProcessingUI.py
@@ -42,8 +42,6 @@ class MyMplCanvas(FigureCanvas):
         FigureCanvas.__init__(self,fig)
         self.setParent(parent)
 
-        print(FigureCanvas)
-
         FigureCanvas.setSizePolicy(self,QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
 
         FigureCanvas.updateGeometry(self)
@@ -54,12 +52,9 @@ class MyMplCanvas(FigureCanvas):
 class MyDynamicMplCanvas(MyMplCanvas):
     def __init__(self,*args,**kwargs):
         MyMplCanvas.__init__(self,*args,**kwargs)
-        timer = QtCore.QTimer(self)
-        timer.timeout.connect(self.update_figure)
-        timer.start(1000)
 
     def compute_initial_figure(self):
-        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
+        self.axes.plot([], [], 'r')
 
     def update_figure(self):
         l = [random.randint(0,10) for i in range(4)]
@@ -70,9 +65,13 @@ class MyDynamicMplCanvas(MyMplCanvas):
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(771, 448)
+        MainWindow.resize(771, 508)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+        self.plotwidget = MyDynamicMplCanvas(self.centralwidget, width = 5, height = 4, dpi = 100)
+        self.plotwidget.setGeometry(QtCore.QRect(40, 40, 381, 241))
+        self.plotwidget.setObjectName("widget")
 
         self.startTimeInput = QtWidgets.QDateTimeEdit(self.centralwidget)
         self.startTimeInput.setGeometry(QtCore.QRect(40, 310, 194, 22))
@@ -85,6 +84,7 @@ class Ui_MainWindow(object):
         self.btn_refresh = QtWidgets.QPushButton(self.centralwidget)
         self.btn_refresh.setGeometry(QtCore.QRect(140, 350, 93, 28))
         self.btn_refresh.setObjectName("btn_refresh")
+        self.btn_refresh.clicked.connect(self.plotwidget.update_figure)
 
         self.btn_parse = QtWidgets.QPushButton(self.centralwidget)
         self.btn_parse.setGeometry(QtCore.QRect(240, 350, 93, 28))
@@ -96,12 +96,34 @@ class Ui_MainWindow(object):
         self.btn_open.clicked.connect(self.open_tdmsfile)
 
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(450, 40, 301, 351))
+        self.textBrowser.setGeometry(QtCore.QRect(450, 40, 301, 250))
         self.textBrowser.setObjectName("textBrowser")
 
-        self.widget = MyDynamicMplCanvas(self.centralwidget, width = 5, height = 4, dpi = 100)
-        self.widget.setGeometry(QtCore.QRect(40, 40, 381, 241))
-        self.widget.setObjectName("widget")
+        self.selectChannel = QtWidgets.QListWidget(self.centralwidget)
+        self.selectChannel.setGeometry(QtCore.QRect(600, 320, 151, 151))
+        self.selectChannel.setProperty("isWrapping", False)
+        self.selectChannel.setResizeMode(QtWidgets.QListView.Fixed)
+        self.selectChannel.setViewMode(QtWidgets.QListView.ListMode)
+        self.selectChannel.setModelColumn(0)
+        self.selectChannel.setWordWrap(False)
+        self.selectChannel.setSelectionRectVisible(False)
+        self.selectChannel.setObjectName("selectChannel")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(620, 290, 91, 20))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(460, 290, 91, 20))
+        self.label_2.setObjectName("label_2")
+        self.selectGroup = QtWidgets.QListWidget(self.centralwidget)
+        self.selectGroup.setGeometry(QtCore.QRect(450, 320, 141, 151))
+        self.selectGroup.setProperty("isWrapping", False)
+        self.selectGroup.setResizeMode(QtWidgets.QListView.Fixed)
+        self.selectGroup.setViewMode(QtWidgets.QListView.ListMode)
+        self.selectGroup.setModelColumn(0)
+        self.selectGroup.setWordWrap(False)
+        self.selectGroup.setSelectionRectVisible(False)
+        self.selectGroup.setObjectName("selectGroup")
+        self.selectGroup.itemClicked.connect(self.updatechannels)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -117,15 +139,16 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MHDLab PostProcessor"))
         self.btn_refresh.setText(_translate("MainWindow", "Refresh"))
         self.btn_parse.setText(_translate("MainWindow", "Parse"))
         self.btn_open.setText(_translate("MainWindow", "Open File"))
 
     def open_tdmsfile(self):
-        name = QtWidgets.QFileDialog.getOpenFileName(MainWindow, 'Open File', 'C:\\')
+        name = QtWidgets.QFileDialog.getOpenFileName(MainWindow, 'Open File', 'C:\\Labview Test Data\\2018-08-16\\Sensors')
         filepath = name[0]
         self.Logfiletdms = TF(filepath)
+
         folder = os.path.split(filepath)
         datefolder = os.path.split(folder[0])
         eventlogpath = os.path.join(datefolder[0],'eventlog.json')
@@ -134,8 +157,28 @@ class Ui_MainWindow(object):
         text = json.dumps(self.jsonfile)
         self.textBrowser.setText(text)
         
+        self.groups = self.Logfiletdms.groups()
+        self.selectGroup.clear()
+        self.selectGroup.insertItems(0,self.groups)
+        self.selectGroup.setCurrentRow(0)
+
+        self.updatechannels()
 
 
+
+        #self.selectGroup.set
+
+
+    def updatechannels(self):
+        selgroup = self.selectGroup.currentRow()
+        
+        channels = self.Logfiletdms.group_channels(self.groups[selgroup])
+        channelnamelist = []
+        for channel in channels:
+            channelnamelist.append(channel.channel)
+        self.selectChannel.clear()
+        self.selectChannel.insertItems(0,channelnamelist)
+        self.selectChannel.setCurrentRow(0)
 
 
 
