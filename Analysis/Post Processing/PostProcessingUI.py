3b90cf62df19caba4958a2b5db4b8a53b4bf73d1
Lee Aspitarte <aspitarte@gmail.com>
2018-09-04 10:56:20 -0700

    switch back to windows

--- Analysis/Post Processing/PostProcessingUI.py
+++ Analysis/Post Processing/PostProcessingUI.py
@@ -318,8 +318,8 @@ ui.setupUi(MainWindow)
 ui.link_buttons()
 MainWindow.show()
 
-#ui.open_tdmsfile('C:\\Labview Test Data\\2018-08-22\\Sensors\\Log_Sensors_DAQ_5.tdms') #Windows
-ui.open_tdmsfile('//home//lee//Downloads//2018-08-22//Sensors//Log_Sensors_DAQ_5.tdms') #Linux
+ui.open_tdmsfile('C:\\Labview Test Data\\2018-08-22\\Sensors\\Log_Sensors_DAQ_5.tdms') #Windows
+#ui.open_tdmsfile('//home//lee//Downloads//2018-08-22//Sensors//Log_Sensors_DAQ_5.tdms') #Linux
 
 
 sys.exit(app.exec_())
