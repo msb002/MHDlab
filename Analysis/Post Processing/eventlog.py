39ec496e176091736a764578947a6148daa2fb13
Lee Aspitarte <aspitarte@gmail.com>
2018-09-04 19:21:08 -0700

    get eventlog class structure working
    startup and shutdown

--- Analysis/Post Processing/eventlog.py
+++ Analysis/Post Processing/eventlog.py
@@ -2,122 +2,128 @@
 import json
 import time
 
-Eventlogfile = ""
+#Eventlogfile = ""
 
-def writeevent(event):
+def writeevent(Eventlogfile, event):
     with open(Eventlogfile, "a") as write_file:
         write_file.write(',\n')
         json.dump(event, write_file)
 
-def initialize():
-    global Eventlogfile
-    if isinstance(Eventlogfile,bytes): #The labview addin passes a bytes instead of string. 
-        Eventlogfile = Eventlogfile.decode("utf-8")
 
-    event = {
-    "dt": time.time(),
-    "event": {
-        "type" : "MonitorVIStarted"
-    } 
-    }
+class Eventlog():
+    def __init__(self,Eventlogfile):
+        #global Eventlogfile
+        self.Eventlogfile = Eventlogfile
+        if isinstance(self.Eventlogfile,bytes): #The labview addin passes a bytes instead of string. 
+            self.Eventlogfile = self.Eventlogfile.decode("utf-8")
 
-    with open(Eventlogfile,'r') as read_file:
-        contents = read_file.read()
+        event = {
+        "dt": time.time(),
+        "event": {
+            "type" : "MonitorVIStarted"
+        } 
+        }
 
-    with open(Eventlogfile,'a') as write_file:
-        if(len(contents) > 0):
-            if(contents[-1] == ']'):
-                length = write_file.seek(0,2)
-                write_file.seek(length-2)
-                write_file.truncate()
-                write_file.write(',\n')
-        else:
-            write_file.write('[\n')
-        json.dump(event, write_file)
+        with open(self.Eventlogfile,'r') as read_file:
+            contents = read_file.read()
 
-def shutdown():
-    event = {
-    "dt": time.time(),
-    "event": {
-        "type" : "MonitorVIClosed"
-    } 
-    }
+        with open(self.Eventlogfile,'a') as write_file:
+            if(len(contents) > 0):
+                if(contents[-1] == ']'):
+                    length = write_file.seek(0,2)
+                    write_file.seek(length-2)
+                    write_file.truncate()
+                    write_file.write(',\n')
+            else:
+                write_file.write('[\n')
+            json.dump(event, write_file)  
 
-    writeevent(event)
+    def shutdown(self):
+        event = {
+        "dt": time.time(),
+        "event": {
+            "type" : "MonitorVIClosed"
+        } 
+        }
 
-    with open(Eventlogfile,'a') as write_file:
-        write_file.write('\n]')
+        writeevent(self.Eventlogfile, event)
+
+        with open(self.Eventlogfile,'a') as write_file:
+            write_file.write('\n]')
 
 
 
 
-def TestCaseInfoChange(TestDataInfo):
-    for idx, string in  enumerate(TestDataInfo):
-        TestDataInfo[idx] = string.decode("utf-8")
 
-    # with open(Eventlogfile,'r') as read_file:
-    #     contents = read_file.read()
 
-    # print(contents)
 
-    # existing_tci_arr = []
-    # for event in contents:
-    #     if event['event']['type'] == 'TestCaseInfoChange':
-    #         eventinfo = event['event']['event info']
-    #         existing_tci_arr.append([eventinfo['project'],eventinfo['subfolder'],eventinfo['filename'],eventinfo['measurementnumber']])
+# def TestCaseInfoChange(TestDataInfo):
+#     for idx, string in  enumerate(TestDataInfo):
+#         TestDataInfo[idx] = string.decode("utf-8")
+
+#     # with open(Eventlogfile,'r') as read_file:
+#     #     contents = read_file.read()
+
+#     # print(contents)
+
+#     # existing_tci_arr = []
+#     # for event in contents:
+#     #     if event['event']['type'] == 'TestCaseInfoChange':
+#     #         eventinfo = event['event']['event info']
+#     #         existing_tci_arr.append([eventinfo['project'],eventinfo['subfolder'],eventinfo['filename'],eventinfo['measurementnumber']])
             
-    # for existing_tci in existing_tci_arr:
-    #     if(existing_tci == TestDataInfo):
-    #         return 0
+#     # for existing_tci in existing_tci_arr:
+#     #     if(existing_tci == TestDataInfo):
+#     #         return 0
     
-    project = TestDataInfo[0]
-    subfolder = TestDataInfo[1]
-    filename = TestDataInfo[2]
-    measurementnumber = TestDataInfo[3]
+#     project = TestDataInfo[0]
+#     subfolder = TestDataInfo[1]
+#     filename = TestDataInfo[2]
+#     measurementnumber = TestDataInfo[3]
 
-    event = {
-    "dt": time.time(),
-    "event": {
-        "type" : "TestCaseInfoChange",
-        "event info": {
-            "project": project,
-            "subfolder": subfolder,
-            "filename": filename,
-            "measurementnumber": measurementnumber 
-            }
-        }
-    }
+#     event = {
+#     "dt": time.time(),
+#     "event": {
+#         "type" : "TestCaseInfoChange",
+#         "event info": {
+#             "project": project,
+#             "subfolder": subfolder,
+#             "filename": filename,
+#             "measurementnumber": measurementnumber 
+#             }
+#         }
+#     }
 
-    writeevent(event)
+#     writeevent(event)
 
-    return 1
-
-
-def RunningVIsChange(VIname,OnOff):
-    event = {
-    "dt": time.time(),
-    "event": {
-        "type" : "VIRunningChange",
-        "event info": {
-            "name" : VIname.decode("utf-8"),
-            "newstate" : OnOff
-            }
-        }
-    }
-
-    writeevent(event)
+#     return 1
 
 
-def SavingVIsChange(VIname,OnOff):
-    event = {
-    "dt": time.time(),
-    "event": {
-        "type" : "VISavingChange",
-        "event info": {
-            "name" : VIname.decode("utf-8"),
-            "newstate" : OnOff
-            }
-        }
-    }
+# def RunningVIsChange(VIname,OnOff):
+#     event = {
+#     "dt": time.time(),
+#     "event": {
+#         "type" : "VIRunningChange",
+#         "event info": {
+#             "name" : VIname.decode("utf-8"),
+#             "newstate" : OnOff
+#             }
+#         }
+#     }
 
-    writeevent(event)
+#     writeevent(event)
+
+
+# def SavingVIsChange(VIname,OnOff):
+#     event = {
+#     "dt": time.time(),
+#     "event": {
+#         "type" : "VISavingChange",
+#         "event info": {
+#             "name" : VIname.decode("utf-8"),
+#             "newstate" : OnOff
+#             }
+#         }
+#     }
+
+#     writeevent(event)
