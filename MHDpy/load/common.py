692afdcb2dc37c99e38ba634e0f45c92b6ac84c9
Lee Aspitarte <aspitarte@gmail.com>
2018-10-14 01:29:42 -0700

    setup loading of thermocouples and powermeter

--- MHDpy/load/common.py
+++ MHDpy/load/common.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import nptdms
 
 def create_tcdict(filepaths, loadfn, prefix = None ):
     """takes in a list of files and a lod function, and creates a dict of a df for each file. If a prefix is passed, that is removed from the filename (typically the instrument name so only the test case is left as the dict key)"""
@@ -18,6 +19,32 @@ def create_tcdict(filepaths, loadfn, prefix = None ):
         dfs[testcase] =df
     
     return dfs
+
+def tdms2df(filepath):
+    tdmsfile = nptdms.TdmsFile(filepath)
+    df = tdmsfile.as_dataframe()
+
+    #test if a waveform channel
+    channel1 = tdmsfile.group_channels(tdmsfile.groups()[0])[0]
+    waveform = True
+    try:
+        channel1.time_track()
+    except KeyError:
+        waveform = False
+    #find the longest waveform
+    if waveform:
+        longestchannel = None
+        length = 0
+        for group in tdmsfile.groups():
+            for channel in tdmsfile.group_channels(group):
+                newlength = len(channel.data)
+                if newlength > length:
+                    length = newlength
+                    longestchannel = channel
+        timedata = longestchannel.time_track(absolute_time = True) 
+        df = df.set_index(timedata)
+
+    return df
 
 
 def _remove_prefix(s, prefix):
